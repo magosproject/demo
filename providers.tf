b7bdc1bbfd5cf411504a35912c899f695b3dbc7a
Bruno Schaatsbergen <git@bschaatsbergen.com>
2026-05-18 19:10:24 +0200

    render magos as output in ascii

--- providers.tf
+++ providers.tf
@@ -1,22 +1,3 @@
 terraform {
   required_version = ">= 1.3.0"
-
-
-  required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "3.8.1"
-    }
-  }
-
-
-  # required_providers {
-  #   google      = ">= 6.5.0"
-  #   google-beta = ">= 6.5.0"
-  # }
-
-  # backend "gcs" {
-  #   bucket  = "magos-tfstate-staging"
-  #   prefix  = "terraform/state"
-  # }
 }
